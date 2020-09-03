0bb39e7b8fc5b6c0cd9ad081934ee022cb56148b
Adrián Gutiérrez Sánchez <adriangutierrez@MacAdrian.local>
2020-09-03 06:29:18 -0500

    Added profiles ViewSet

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework.authentication import TokenAuthentication
 
 from rest_framework import viewsets
+from profiles_api import models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """TEST API VIEW"""
@@ -81,4 +85,10 @@ class HelloViewset(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """For removing an object"""
         return Response({'http_method': 'DELETE'})
-        
+
+class UserProfileViewset(viewsets.ModelViewSet):
+    """Handle updating and creating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
