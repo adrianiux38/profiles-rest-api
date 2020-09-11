ec6da21263cdd7f6f3e3d02f8ee75c5ad392c891
Adrián Gutiérrez Sánchez <adriangutierrez@MacAdrian.local>
2020-09-11 16:26:33 -0500

    Added user profile feed API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewset, base_name="hello-viewset")
 router.register('profile', views.UserProfileViewset)
+router.register('feed', views.UserProfileFeedViewSet)
 
 
 urlpatterns = [
